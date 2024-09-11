9700135dae8e4ce9eeb10ff636887531e8ab6256
DHIWAHAR-K <adhithyak99@gmail.com>
2024-09-11 13:21:49 -0400

    files added and updated

--- convert_labels.py
+++
@@ -1,8 +0,0 @@
-def convert_label(item, name): 
-    items = list(map(float, item.split()))
-    label = ""
-    for idx in range(len(items)): 
-        if items[idx] == 1: 
-            label += name[idx] + " "
-    
-    return label.strip()
